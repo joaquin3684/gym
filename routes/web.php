b77a5e6b4c4fc9c450b99f0d5ef0722ce0f40d58
vagrant <vagrant@homestead>
2018-09-10 20:21:48 +0000

    lkjdsljf

--- routes/web.php
+++ routes/web.php
@@ -19,28 +19,3 @@ Route::get('prueba', function() {
 
 });
 
-
-//SOCIO
-
-Route::post('socio/crear', 'SocioController@store');
-Route::post('socio/comprar', 'SocioController@comprar');
-Route::get('socio/all', 'SocioController@all');
-Route::put('socio/editar/{id}', 'SocioController@update');
-Route::get('socio/find/{id}', 'SocioController@show');
-
-
-//CAJA
-
-Route::post('caja/ingres', 'CajaController@ingreso');
-Route::post('caja/egreso', 'CajaController@egreso');
-Route::post('caja/movimientos', 'CajaController@movimientos');
-
-
-//VENDIBLES
-
-Route::get('vendibles/clases', 'VendiblesController@clases');
-Route::get('vendibles/articulos', 'VendiblesController@articulos');
-
-//VENTAS
-
-Route::post('ventas/all', 'VentaController@ventas');
